459dcbd268526baf000ad77ad0860902092301a7
Fabian Affolter <mail@fabian-affolter.ch>
2019-05-01 01:21:50 +0200

    Fix typos and a little cleanup

--- masscan/example.py
+++ masscan/example.py
@@ -8,17 +8,18 @@ import masscan
 try:
     mas = masscan.PortScanner()
 except masscan.PortScannerError:
-    print('Masscan not found', sys.exc_info()[0])
+    print("masscan binary not found", sys.exc_info()[0])
     sys.exit(1)
 except:
     print("Unexpected error:", sys.exc_info()[0])
     sys.exit(1)
 
-print('maascan version: ', mas.masscan_version)
-mas.scan('172.0.8.78', ports='U:445,U:53')
-print('maascan command line: ', mas.command_line)
+print("masscan version:", mas.masscan_version)
+mas.scan('127.0.0.1', ports='U:445,U:53')
+print("masscan command line:", mas.command_line)
 #print('maascan scaninfo: ', mas.scaninfo)
 #print('maascan scanstats: ', mas.scanstats)
 
 for host in mas.all_hosts:
-    print('Host: %s (%s)' % (host, mas[host]))
+    print("Host: %s (%s)" % (host, mas[host]))
+
