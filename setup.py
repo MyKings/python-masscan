a33ba5241f4de1eb875e442d4a3b34909368939c
MyKings <xsseroot@gmail.com>
2019-10-15 15:36:22 +0800

    fixed #26

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env
 # -*- coding: utf-8 -*-
 import os
 import sys
@@ -16,12 +16,8 @@ IS_PY2 = sys.version_info[0] == 2
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-if IS_PY2:
-    with io.open(os.path.join(here, 'README.rst')) as readme_file:
-        long_description = readme_file.read()
-else:
-    with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
-        long_description = readme_file.read()
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
 
 setup(
     name='python-masscan',
