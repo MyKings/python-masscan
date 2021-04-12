ce8602d0072e5d959bff5456633476ff16b1b275
KOLANICH <kolan_n@mail.ru>
2021-04-12 13:00:41 +0300

    Moved all the metadata into setup.cfg

--- masscan/__init__.py
+++ masscan/__init__.py
@@ -2,7 +2,8 @@
 """Init file for the masscan bindings."""
 from .masscan import *
 
+from .version import __version__
+
 __author__ = 'MyKings (xsseroot@gmail.com)'
-__version__ = '0.1.6'
 __last_modification__ = '2019.10.15'
 
--- setup.py
+++ setup.py
@@ -1,52 +1,7 @@
-#!/usr/bin/env
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os
-import sys
-import io
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
-from masscan import __version__
-from masscan import __author__
-
-IS_PY2 = sys.version_info[0] == 2
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
-    long_description = readme_file.read()
-
-setup(
-    name='python-masscan',
-    version=__version__,
-    author=__author__,
-    author_email='xsseroot@gmail.com',
-    license='GPLv3+',
-    keywords="masscan, portscanner",
-    platforms=[
-        "Operating System :: OS Independent",
-        ],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Topic :: System :: Monitoring",
-        "Topic :: System :: Networking",
-        "Topic :: System :: Networking :: Firewalls",
-        "Topic :: System :: Networking :: Monitoring",
-        ],
-    packages=['masscan'],
-    url='https://github.com/MyKings/python-masscan',
-    long_description=long_description,
-    long_description_content_type="text/markdown"
-)
+if __name__ == "__main__":
+    setup()
